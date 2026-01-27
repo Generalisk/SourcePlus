107d940581fb87a97e2ec164e94978b63d6d6cf8
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-01-27 14:37:18 +0000

    Setup ImGui

--- src/Editor/Program.cs
+++ src/Editor/Program.cs
@@ -1,11 +1,17 @@
-﻿using static Raylib_cs.Raylib;
+﻿using rlImGui_cs;
+
+using static Raylib_cs.Raylib;
 
 InitWindow(1280, 720, "Source+");
+
+rlImGui.Setup(true, true);
 
 while (!WindowShouldClose())
 {
     BeginDrawing();
     EndDrawing();
 }
+
+rlImGui.Shutdown();
 
 CloseWindow();
