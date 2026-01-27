a0aaedd4634ee53b7350d0255e0bea766feeb583
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-01-27 14:35:27 +0000

    Setup Raylib

--- src/Editor/Program.cs
+++ src/Editor/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Raylib_cs.Raylib;
+
+InitWindow(1280, 720, "Source+");
+
+while (!WindowShouldClose())
+{
+    BeginDrawing();
+    EndDrawing();
+}
+
+CloseWindow();
