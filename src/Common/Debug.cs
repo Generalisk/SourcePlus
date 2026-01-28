c98fe50cbe953c2916fb9903e9f3dfc98b6bddd1
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-01-28 07:54:25 +0000

    Add debug log script

---
+++ src/Common/Debug.cs
@@ -0,0 +1,25 @@
+﻿global using static SourcePlus.Debug;
+
+namespace SourcePlus;
+
+internal static class Debug
+{
+    public static void Log(string text, params object[] args)
+    {
+        Console.WriteLine(text, args);
+    }
+
+    public static void LogWarning(string text, params object[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(text, args);
+        Console.ResetColor();
+    }
+
+    public static void LogError(string text, params object[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(text, args);
+        Console.ResetColor();
+    }
+}
