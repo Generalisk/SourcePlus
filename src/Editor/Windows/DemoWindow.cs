9e8153e5b568eb91dcd1e0281541838dcb452d0b
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-02-03 07:15:48 +0000

    Add ImGui demo window
    I bet your asking "why implement it now when you already have 2 finished windows?" Well, my friend, I honestly do not know. I just kinda forgot it existed up 'till this point.

---
+++ src/Editor/Windows/DemoWindow.cs
@@ -0,0 +1,37 @@
+﻿#if DEBUG
+using ImGuiNET;
+
+namespace SourcePlus.Editor.Windows;
+
+internal class DemoWindow : Window
+{
+    public override string Name { get; } = "Demo Window";
+
+    private bool open = true;
+
+    internal override void UpdateInternal(out bool closed)
+    {
+        Update();
+
+        closed = false;
+
+        if (!open)
+        {
+            Dispose();
+            closed = true;
+            return;
+        }
+    }
+
+    internal override void DrawInternal()
+    {
+        ImGui.SetNextWindowSizeConstraints(MinSize, MaxSize);
+        ImGui.ShowDemoWindow(ref open);
+    }
+
+    protected override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+}
+#endif // DEBUG
