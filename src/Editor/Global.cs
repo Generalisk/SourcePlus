ed9b443bfe37d9a083e61b6509bfbb9c79d93602
Generalisk <80116907+Generalisk@users.noreply.github.com>
2026-02-12 23:59:58 +0000

    Tweak protection levels in Global.cs

--- src/Editor/Global.cs
+++ src/Editor/Global.cs
@@ -9,13 +9,15 @@ internal static class Global
     /// <summary>
     /// The local path of the active project
     /// </summary>
-    public static string ProjectPath { get; set; } = "";
+    public static string ProjectPath { get; internal set; } = "";
 
-    public static List<Window> Windows { get; set; } = new List<Window>();
 
-    public static bool WindowExists<T>() => WindowExists(typeof(T));
 
-    public static bool WindowExists(Type type)
+    internal static List<Window> Windows { get; set; } = new List<Window>();
+
+    internal static bool WindowExists<T>() => WindowExists(typeof(T));
+
+    internal static bool WindowExists(Type type)
     {
         foreach (var window in Windows)
             if (window.GetType() == type)
